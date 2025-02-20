604b181a9fe4453d11722f31c7c3fc73a0dba452
Chano <marcomangpang@gmail.com>
2025-02-20 20:17:56 +0800

    TESTING

--- app/src/main/java/com/example/oneshot/fragments/SearchPage.java
+++ app/src/main/java/com/example/oneshot/fragments/SearchPage.java
@@ -13,11 +13,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.oneshot.MangaAdapter;
 import com.example.oneshot.R;
+import com.example.oneshot.model.Manga;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchPage extends Fragment {
-
+    private RecyclerView recyclerViewManga;
+    private MangaAdapter mangaAdapter;
+    private List<Manga> mangaList;
+    private DatabaseReference databaseReference;
     View view;
     @Nullable
     @Override
@@ -25,8 +41,41 @@ public class SearchPage extends Fragment {
         super.onCreate(savedInstanceState);
 
         View view = inflater.inflate(R.layout.fragment_search_page, container, false);
+        recyclerViewManga = view.findViewById(R.id.recyclerViewManga);
+        recyclerViewManga.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        mangaList = new ArrayList<>();
+        mangaAdapter = new MangaAdapter(getContext(), mangaList, this::onMangaClick);
+        recyclerViewManga.setAdapter(mangaAdapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Manga");
+        fetchMangas();
 
         return view;
 
     }
+    private void fetchMangas() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mangaList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Manga manga = dataSnapshot.getValue(Manga.class);
+                    mangaList.add(manga);
+                }
+                mangaAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+    private void onMangaClick(Manga manga) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new ChapterListPage(manga));
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
+}
+
